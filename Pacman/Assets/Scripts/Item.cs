6757b6a945ce794cabb5af70d1074124b004f9a5
GusOs <gus_92@hotmail.es>
2021-01-21 15:36:43 +0100

    Add player movement

---
+++ Pacman/Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //Colision
+    private Collider itemCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemCollision = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider itemCollision)
+    {
+        if (itemCollision.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Velocidad del jugador
+    public float speed = 2.0f;
+
+    private Vector2 direction = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        Move();
+    }
+
+    //Comprobar inputs
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+
+        } else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+
+        } else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+
+        } else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+    }
+
+    //Movimiento del jugador
+    void Move()
+    {
+        transform.position += (Vector3)(direction * speed) * Time.deltaTime;
+    }
+}
