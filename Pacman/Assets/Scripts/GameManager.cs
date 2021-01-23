e8b981eb47f95947b421495e7817eac67d264b9e
GusOs <gus_92@hotmail.es>
2021-01-23 19:45:32 +0100

    Add scene manager and gameplay manager

---
+++ Pacman/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Instancia de la clase
+    public static GameManager Instance;
+
+    // Audio al morir el jugador
+    public Sound PlayerDeath;
+
+    // Comprobar si el estado del juego está activo
+    public bool isGameActive;
+
+    //Objeto del panel wingame
+    public GameObject panelWinGame;
+
+    private void Awake()
+    {
+        Instance = this;
+        isGameActive = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GameOver()
+    {
+        if(isGameActive)
+        {
+            isGameActive = false;
+            AudioManager.Instance.PlaySound(PlayerDeath);
+            SceneManager.LoadScene("Pacman");
+        }
+    }
+
+    public void GameWin()
+    {
+        //todos los items a set active false
+        if(isGameActive)
+        {
+            isGameActive = false;
+            StartCoroutine(ShowGameWinPanelCoroutine());
+            SceneManager.LoadScene("Pacman");
+        }
+    }
+
+    public IEnumerator ShowGameWinPanelCoroutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+        panelWinGame.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
